c99a3d9cf2d5a301dd23bcc5f90bce774276097b
JingqingZ <zhangjqsmiling@gmail.com>
2016-04-23 20:11:13 +0800

    lab

--- labcodes/lab5/kern/trap/trap.c
+++ labcodes/lab5/kern/trap/trap.c
@@ -53,6 +53,9 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
+     /* LAB5 2012011380 */
+     //you should update your lab1 code (just add ONE or TWO lines of code), let user app to use syscall to get the service of ucore
+     //so you should setup the syscall interrupt gate in here
     extern uintptr_t __vectors[];
     for (int i = 0 ; i < 256 ; i++) {
         SETGATE(idt[i], 0, KERNEL_CS, __vectors[i], DPL_KERNEL);
@@ -60,9 +63,6 @@ idt_init(void) {
     SETGATE(idt[T_SYSCALL], 1, KERNEL_CS, __vectors[T_SYSCALL], DPL_USER);
     SETGATE(idt[T_SWITCH_TOK], 1, KERNEL_CS, __vectors[T_SWITCH_TOK], DPL_USER);
     lidt(&idt_pd);
-     /* LAB5 YOUR CODE */ 
-     //you should update your lab1 code (just add ONE or TWO lines of code), let user app to use syscall to get the service of ucore
-     //so you should setup the syscall interrupt gate in here
 }
 
 static const char *
@@ -207,7 +207,7 @@ trap_dispatch(struct trapframe *tf) {
                     panic("handle pgfault failed in kernel mode. ret=%d\n", ret);
                 }
                 cprintf("killed by kernel.\n");
-                panic("handle user mode pgfault failed. ret=%d\n", ret); 
+                panic("handle user mode pgfault failed. ret=%d\n", ret);
                 do_exit(-E_KILLED);
             }
         }
@@ -218,7 +218,7 @@ trap_dispatch(struct trapframe *tf) {
     case IRQ_OFFSET + IRQ_TIMER:
 #if 0
     LAB3 : If some page replacement algorithm(such as CLOCK PRA) need tick to change the priority of pages,
-    then you can add code here. 
+    then you can add code here.
 #endif
 		/* LAB1 2012011380 : STEP 3 */
 		/* handle the timer interrupt */
@@ -226,15 +226,16 @@ trap_dispatch(struct trapframe *tf) {
 		* (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
 		* (3) Too Simple? Yes, I think so!
 		*/
-		ticks ++;
-		if (ticks % TICK_NUM == 0) {
-			print_ticks();
-		}
-        /* LAB5 YOUR CODE */
+        /* LAB5 2012011380 */
         /* you should upate you lab1 code (just add ONE or TWO lines of code):
          *    Every TICK_NUM cycle, you should set current process's current->need_resched = 1
          */
-  
+		ticks ++;
+		if (ticks % TICK_NUM == 0) {
+			//print_ticks();
+            current->need_resched = 1;
+		}
+
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
@@ -281,11 +282,11 @@ trap(struct trapframe *tf) {
         // keep a trapframe chain in stack
         struct trapframe *otf = current->tf;
         current->tf = tf;
-    
+
         bool in_kernel = trap_in_kernel(tf);
-    
+
         trap_dispatch(tf);
-    
+
         current->tf = otf;
         if (!in_kernel) {
             if (current->flags & PF_EXITING) {
